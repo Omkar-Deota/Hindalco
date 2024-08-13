27ddc7fec06dffee440d14e3d85b12dd5c78f6b6
Omkar-Deota <omkarinshu2003@gmail.com>
2024-08-13 17:54:20 +0530

    port added

--- Server/app.js
+++ Server/app.js
@@ -17,6 +17,7 @@ app.use(cors({
 // Create a MySQL connection pool
 const pool = mysql.createPool({
   connectionLimit: 10,
+  port:process.env.DB_PORT,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
